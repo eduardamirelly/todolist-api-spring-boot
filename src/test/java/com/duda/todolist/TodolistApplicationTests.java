a9b6d26f10e8fd82658d2e4b1077f51e2e4c32d7
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-10-11 19:40:55 -0300

    🎉 setup java project

---
+++ src/test/java/com/duda/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duda.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
